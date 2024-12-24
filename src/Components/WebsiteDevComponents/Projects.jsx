2480197df9109498e65bbd48e631f3827877024f
remeessalam <remeesreme4u@gmail.com>
2024-12-24 18:10:47 +0530

    clicking redirect to web development page done

--- src/Components/WebsiteDevComponents/Projects.jsx
+++ src/Components/WebsiteDevComponents/Projects.jsx
@@ -1,6 +1,11 @@
+import { useNavigate } from "react-router-dom";
 import { projects } from "../../constant";
 
 const Projects = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate("/web-development");
+  };
   return (
     <div className="min-h-screen bg-black px-4 sm:px-6 lg:px-8 py-16">
       <div className="max-w-7xl mx-auto">
@@ -9,7 +14,8 @@ const Projects = () => {
             <div
               data-aos="fade-up"
               key={index}
-              className={`group relative ${
+              onClick={handleClick}
+              className={`group relative  ${
                 index % 2 === 0 ? "md:pr-8" : "md:pl-8 md:pt-40"
               }`}
             >
@@ -17,15 +23,15 @@ const Projects = () => {
                 <img
                   src={project.image}
                   alt={project.title}
-                  className="w-full h-auto object-cover transition-transform duration-300 group-hover:scale-105"
+                  className="w-full h-auto cursor-pointer object-cover transition-transform duration-300 group-hover:scale-105"
                 />
               </div>
               <div className="mt-4 flex items-start justify-between">
                 <div className="flex flex-col">
-                  <h3 className="text-white text-lg font-medium pr-4">
+                  <h3 className="text-white cursor-pointer text-lg font-medium pr-4">
                     {project.title}
                   </h3>
-                  <p className="desc">{project.desc}</p>
+                  <p className="desc cursor-pointer">{project.desc}</p>
                 </div>
                 {/* <div className="flex-shrink-0 w-6 h-6 rounded-full bg-yellow-400 flex items-center justify-center">
                   <span className="text-black text-sm font-bold">
